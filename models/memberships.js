9320a04308693dd3a9f2384fe1673706e25909cd
Outsider <outsideris@gmail.com>
2018-09-17 13:31:12 +0900

    add membership status for verifying indendity

--- models/memberships.js
+++ models/memberships.js
@@ -1,13 +1,13 @@
 const { Enum } = require('enumify');
 
 class Status extends Enum {}
-Status.initEnum(['active', 'pending', 'deleted']);
+Status.initEnum(['active', 'pending', 'verified', 'deleted']);
 
 module.exports = (sequelize, DataTypes) => {
   const Membership = sequelize.define('Membership', {
     publicAddress: { type: DataTypes.STRING(60), allowNull: false, unique: true },
     applicantId: { type: DataTypes.STRING(40), allowNull: false, unique: true },
-    status: { type: DataTypes.ENUM, values: ['active', 'pending', 'deleted'], defaultValue: Status.pending.name },
+    status: { type: DataTypes.ENUM, values: ['active', 'pending', 'verified', 'deleted'], defaultValue: Status.pending.name },
   }, {
     tableName: 'memberships',
     paranoid: true,
