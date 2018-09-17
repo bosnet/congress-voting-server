aeb39b83cd0c33b6b99e714f0e13c969f34ecf8c
Outsider <outsideris@gmail.com>
2018-09-17 14:27:09 +0900

    return errors as JSON not text/html

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const logger = require('morgan');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,14 +20,12 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  // TODO: error logging(sentry?)
+  res.status(status).json({
+    error: err.message,
+  });
 });
 
 module.exports = app;
