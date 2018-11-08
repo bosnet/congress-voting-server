767dc572f4695766d5d009ddeee0b47183912353
K <zzim2x@gmail.com>
2018-11-08 15:24:09 +0900

    too many await code

--- models/proposals.js
+++ models/proposals.js
@@ -21,65 +21,58 @@ module.exports = (sequelize, DataTypes) => {
   Proposal.register = async function register(m) {
     const exist = await this.findOne({ where: { hash: m.hash } });
     if (!exist) {
-      const d = await this.build(m).save();
-      return d;
+      return this.build(m).save();
     }
     return null;
   };
 
   Proposal.findByHash = async function findByHash(hash) {
-    const d = await this.findOne({ where: { hash } });
-    return d;
+    return this.findOne({ where: { hash } });
   };
 
   Proposal.findById = async function findById(id) {
-    const d = await this.findOne({ where: { id } });
-    return d;
+    return this.findOne({ where: { id } });
   };
 
   Proposal.list = async function list() {
-    const prs = await this.findAll({});
-    return prs;
+    return this.findAll({});
   };
 
   Proposal.listToReport = async function listToReport(currentBlock = 0) {
-    const prs = await this.findAll({
+    return this.findAll({
       where: {
         reported: false,
         end: { [sequelize.Op.lt]: currentBlock },
       },
     });
-    return prs;
   };
 
   Proposal.listToConfirm = async function listToConfirm(currentBlock = 0) {
-    const prs = await this.findAll({
+    return this.findAll({
       where: {
         reported: true,
         reportConfirmed: false,
         end: { [sequelize.Op.lt]: currentBlock },
       },
     });
-    return prs;
   };
 
   Proposal.listOpened = async function listOpened(currentBlock = 0) {
-    const prs = await this.findAll({
+    return this.findAll({
       where: {
         start: { [sequelize.Op.lte]: currentBlock },
         end: { [sequelize.Op.gte]: currentBlock },
       },
     });
-    return prs;
   };
 
   // instance methods
   Proposal.prototype.report = async function report() {
-    await this.update({ reported: true });
+    return this.update({ reported: true });
   };
 
   Proposal.prototype.confirmReport = async function confirmReport() {
-    await this.update({ reportConfirmed: true });
+    return this.update({ reportConfirmed: true });
   };
 
   return Proposal;
