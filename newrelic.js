14a6ffb9403378c237a5287e9ca66a29723af63a
Outsider <outsideris@gmail.com>
2018-11-13 18:35:20 +0900

    add new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+'use strict'
+exports.config = {
+  app_name: [''],
+  license_key: '',
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+}
+
